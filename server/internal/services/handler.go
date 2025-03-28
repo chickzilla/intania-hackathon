8e603f7f6436f4651de5b049752b0350e77dc188
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2025-03-28 23:21:05 +0700

    fix conflict

--- server/internal/services/handler.go
+++ server/internal/services/handler.go
@@ -2,22 +2,22 @@ package services
 
 import (
 	"hackathon/internal/services/auth"
-	"hackathon/internal/services/user"
 	"hackathon/internal/services/course"
+	"hackathon/internal/services/user"
 
 	"gorm.io/gorm"
 )
 
 type Handler struct {
-	Auth *auth.Resolver
-	User *user.Resolver
+	Auth   *auth.Resolver
+	User   *user.Resolver
 	Course *course.Resolver
 }
 
 func NewHandler(db *gorm.DB) *Handler {
 	return &Handler{
-		User: user.NewResolver(db),
-		Auth: auth.NewResolver(db),
+		User:   user.NewResolver(db),
+		Auth:   auth.NewResolver(db),
 		Course: course.NewResolver(db),
 	}
 }
