e1e5cc61fdf47c8a2f79caa4e5507510bfea5f22
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2025-03-28 20:07:32 +0700

    model user, achieve, user_achieve

--- server/internal/models/User.go
+++ server/internal/models/User.go
@@ -2,20 +2,42 @@ package models
 
 import (
 	"hackathon/internal/utils"
+	"time"
 
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
+const (
+	StudentRole  string = "student"
+	LecturerRole string = "lecturer"
+	AdminRole    string = "admin"
+)
+
 type User struct {
-	ID       uuid.UUID `gorm:"type:uuid;primary_key;default:gen_random_uuid()"`
-	Username string    `gorm:"not null"`
-	Password string    `gorm:"not null"`
-	Email    string    `gorm:"not null"`
-	Tel      string    `gorm:""`
+	ID        uuid.UUID `gorm:"type:uuid;primary_key;default:gen_random_uuid()"`
+	Name      string    `gorm:"type:varchar(50);not null"`
+	Email     string    `gorm:"type:varchar(100);not null"`
+	Password  string    `gorm:"type:varchar(100);not null"`
+	UserRole  string    `gorm:"type:user_role;not null"`
+	RankPoint int64     `gorm:"type:int;not null"`
+	Point     int64     `gorm:"type:int;not null"`
+	CreatedAt time.Time `gorm:"type:timestamp;default:now()"`
+	UpdatedAt time.Time `gorm:"type:timestamp;default:now()"`
 }
 
 func MigrateUser(db *gorm.DB) error {
+	if err := db.Exec(`
+		DO $$
+		BEGIN
+			IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_role') THEN
+				CREATE TYPE user_role AS ENUM ('STUDENT', 'LECTURER', 'ADMIN');
+			END IF;
+		END$$;
+	`).Error; err != nil {
+		return err
+	}
+
 	return db.AutoMigrate(&User{})
 }
 
---
+++ server/internal/models/achievement.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"hackathon/internal/utils"
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Achievement struct {
+	ID        uuid.UUID `gorm:"type:uuid;primary_key;default:gen_random_uuid()"`
+	Name      string    `gorm:"type:varchar(50)"`
+	Detail    string    `gorm:"type:varchar(255)"`
+	Level     int       `gorm:"type:int;not null"`
+	CreatedAt time.Time `gorm:"type:timestamp;default:now()"`
+	UpdatedAt time.Time `gorm:"type:timestamp;default:now()"`
+	CreatedBy uuid.UUID `gorm:"type:uuid;not null"`
+	UpdatedBy uuid.UUID `gorm:"type:uuid;not null"`
+}
+
+func MigrateAchievement(db *gorm.DB) error {
+	return db.AutoMigrate(&Achievement{})
+}
+
+func init() {
+	utils.RegisterMigrations(MigrateAchievement)
+}
