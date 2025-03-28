6b05ef619d8ce1b50eed55834b3c150f77ae5db1
max <153589599+m4xkub@users.noreply.github.com>
2025-03-29 00:48:08 +0700

    fix big

--- server/internal/models/Contest.go
+++ server/internal/models/Contest.go
@@ -1,15 +1,25 @@
 package models
 
 import (
+	"hackathon/internal/utils"
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
-type Contest struct {
+type Contests struct {
 	ID      uuid.UUID `gorm:"type:uuid;primary_key;default:gen_random_uuid()"`
-	Name    string
-	Detail  string
-	StartAt time.Time
-	EndAt   time.Time
+	Name    string    `gorm:"not null"`
+	Detail  string    `gorm:"not null"`
+	StartAt time.Time `gorm:"column:start_at;not null"`
+	EndAt   time.Time `gorm:"column:end_at;not null"`
+}
+
+func MigrateContests(db *gorm.DB) error {
+	return db.AutoMigrate(&Contests{})
+}
+
+func init() {
+	utils.RegisterMigrations(MigrateContests)
 }
