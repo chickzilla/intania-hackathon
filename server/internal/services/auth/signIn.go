e7b130e27f11bb17dedddef31a39d757ea9897e8
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2025-03-30 04:39:44 +0700

    add role

--- server/internal/services/auth/signIn.go
+++ server/internal/services/auth/signIn.go
@@ -40,7 +40,7 @@ func (r *Resolver) SignIn(c *gin.Context) {
 		return
 	}
 
-	c.JSON(200, gin.H{"token": token, "id": user.ID})
+	c.JSON(200, gin.H{"token": token, "id": user.ID, "role": user.UserRole})
 }
 
 func GenerateKey(id uuid.UUID, userRole string) (string, error) {
