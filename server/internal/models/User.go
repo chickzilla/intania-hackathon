38365c11c8bd2b626fe63a8d6c13e3e2f753437a
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2025-03-29 17:44:40 +0700

    fix migrate role

--- server/internal/models/User.go
+++ server/internal/models/User.go
@@ -19,7 +19,7 @@ type User struct {
 	FullName  string     `gorm:"type:varchar(50);not null"`
 	Email     string     `gorm:"type:varchar(100);not null"`
 	Password  string     `gorm:"type:varchar(100);not null"`
-	UserRole  string     `gorm:"type:user_role;not null"`
+	UserRole  string     `gorm:"type:varchar(10);not null"`
 	RankPoint int64      `gorm:"type:int;not null"`
 	Point     int64      `gorm:"type:int;not null"`
 	CreatedAt *time.Time `gorm:"type:timestamp;default:now()"`
@@ -27,19 +27,22 @@ type User struct {
 }
 
 func MigrateUser(db *gorm.DB) error {
-	if err := db.Exec(`
-		DO $$
-		BEGIN
-			IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_role') THEN
-				CREATE TYPE user_role AS ENUM ('STUDENT', 'LECTURER', 'ADMIN');
-			END IF;
-		END$$;
-	`).Error; err != nil {
-		return err
-	}
-
 	return db.AutoMigrate(&User{})
 }
+
+/* func MigrateUserRoleEnum(db *gorm.DB) error {
+	return db.Exec(`
+		DO $$
+		BEGIN
+			IF NOT EXISTS (
+				SELECT 1 FROM pg_type WHERE typname = 'user_role'
+			) THEN
+				CREATE TYPE user_role AS ENUM ('STUDENT', 'LECTURER', 'ADMIN');
+			END IF;
+		END
+		$$;
+	`).Error
+} */
 
 func init() {
 	utils.RegisterMigrations(MigrateUser)
